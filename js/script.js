efef0ed7483f77f41f52f3ecb546220a247b4c81
Olivier <olivier.reboul@gmail.com>
2022-04-12 11:08:34 +0200

    navbar complet

---
+++ js/script.js
@@ -0,0 +1,54 @@
+function animeNavBar(){
+    const olivierRLink = document.getElementById("linkOlivierR");
+    const competencesLink = document.getElementById("linkCompetences");
+    const expeLink = document.getElementById("linkExpe");
+    const contactLink = document.getElementById("linkContact");
+    const formationLink = document.getElementById("linkFormations");
+    const lienLink = document.getElementById("linkFooter");
+
+    olivierRLink.addEventListener("click", () =>{
+        alert("Olivier");
+    })
+
+    competencesLink.addEventListener("click", () =>{
+        competencesLink.classList.add('active');
+        expeLink.classList.remove('active');
+        contactLink.classList.remove('active');
+        formationLink.classList.remove('active');
+        lienLink.classList.remove('active');
+    })
+
+    expeLink.addEventListener("click", () =>{
+        competencesLink.classList.remove('active');
+        expeLink.classList.add('active');
+        contactLink.classList.remove('active');
+        formationLink.classList.remove('active');
+        lienLink.classList.remove('active');
+    })
+
+    contactLink.addEventListener("click", () =>{
+        competencesLink.classList.remove('active');
+        expeLink.classList.remove('active');
+        contactLink.classList.add('active');
+        formationLink.classList.remove('active');
+        lienLink.classList.remove('active');
+    })
+
+    formationLink.addEventListener("click", () =>{
+        competencesLink.classList.remove('active');
+        expeLink.classList.remove('active');
+        contactLink.classList.remove('active');
+        formationLink.classList.add('active');
+        lienLink.classList.remove('active');
+    })
+
+    lienLink.addEventListener("click", () =>{
+        competencesLink.classList.remove('active');
+        expeLink.classList.remove('active');
+        contactLink.classList.remove('active');
+        formationLink.classList.remove('active');
+        lienLink.classList.add('active');
+    })
+}
+
+animeNavBar();
